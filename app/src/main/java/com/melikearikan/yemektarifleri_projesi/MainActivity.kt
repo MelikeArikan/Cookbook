5710e0e1ac9459b5f441a0d9c5aa9279a11b2148
Melike ARIKAN <melikearikandev@gmail.com>
2022-08-29 00:31:15 +0300

    next commit

--- app/src/main/java/com/melikearikan/yemektarifleri_projesi/MainActivity.kt
+++ app/src/main/java/com/melikearikan/yemektarifleri_projesi/MainActivity.kt
@@ -10,74 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // mainActivity içerisine 2 tane fragment koyucaz.bir tanesi kaydettiğimiz yemek tariflerine listelediğimiz liste(recyclerView kullanıcaz.)
-        // 2. fragment'da yemek tariflerinin detaylarını gösteren kısım olucak.
-        // kaydettiğimiz yemek listesinin listeFragment da göstericez.
-        // tarifFragment içerisinde yemek tariflerimizin detaylı anlatımı olucak.
-        // listeyi oluşturduğum yerde birtek recyclerView olucak.recyclerView'un id'si mutlaka olmalı.
-        // mainActivity içerisine navHost koyucaz yani fragmentları buraya bağlıcaz.
-        // tarifFragment da 2 tane editText bir tane de yemek görselini koymak için imageView kullanmamız lazım.
-        // kullanıcı görsele tıklaması gerektiğini anlaması lazım.sıfırdan bir görsel oluşturmak için kullanabileceğimiz bir sürü araç var.
-        // görsel yapma aracı olarak ya da tasarım aracı olarak 'adobe xd' kullanılabilir.
-        // activityler arası veri aktarımı yapabilmek için safe arguments'ı kullanıcaz(navigation kütüphanesi.)
-        // navigasyon kütüphanesini ekledikten sonra res altına bir navigasyon grafiği oluşturyorduk.
-        // bir menu yardımı ile tarifFragment'a ulaşmam lazım.
-
-
-        /*
-        ***************************** menu(obsiyon menüsü) nasıl oluşturulur? **************************************
-        res sağ tık >> new >> directory(klasör) ve klasör adı : menu olsun
-        * menu klasörü içinde menü olucağını android studyo anlar zaten. klasör adından anlıyor.
-        * menu ' ye sağ tık >> new  >> menu resource file' a tıklıyoruz.
-        * açılan pencrede root elemant menu olmalı.
-        * file name adına menu adı seçmeliyiz. file name : yemek_ekle gibi
-        * menu olarak yemek_ekle.xml layout dosyası oluşuyor.
-        * menülerle çalışırken xml ile çalışmak daha kolaydır.
-        * menüyü activity'e ekliyoruz.ve bunu yapmak için 2 tane fonksiyon çağırmamız gerkiyor.
-        *
-        *
-         */
-
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        var menuInfalter = menuInflater  // bu kodu kullanarak menülerimizi istediğimiz yerlere bağlayaibiliyoruz.
+        var menuInfalter = menuInflater
         menuInfalter.inflate(R.menu.yemek_ekle,menu)
         return super.onCreateOptionsMenu(menu)
-        //menüyü buraya bağlıcaz
-        // options menu zaten bizim menümüz.
-        // bir xml ile kodu bağlarken inflater kullanıyorduk:
-        //.inflate bizden 2 tane parametre ister : 1)_menu resource'umuz , 2)_hangi menü ile bağlayacağını soruyor,bu bize zaten fonksiyon parametresi olarak verilmiş.
-       // eğer sorun olursa bağlamada file >> invalidate caches ' e bas bazen android içinde bug olabiliyor ve çıkmıyor. bu android studyoyu yeniden başlatır.
-        // kodlarını return üstüne yaz
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // basılan item'ın idsini kontrol ediyoruz.birden fazla item varsa karışıklık olmasın diye.
-        // biz itemımızı oluştururken itemin id'sine yemek_ekleme_item veemiştik.
-        //item.itemID kullanıcının tıkladoğı yerin id'si. bizim istediğimiz item'ın id'sine eşit mi bakıyorz.
 
         if (item.itemId == R.id.yemek_ekleme_item){
-            val action = listeFragmentDirections.actionListeFragmentToTarifFragment("menudengeldim",id = 0)  //tarifFragment'a giderken bilgi ekledik.  //id göndermemize gerek yok öylesine id verdim.
-            // listeFragmentDirections, navigasyonun bizim için oluşturuduğu talimatlar sınıfıydı.
+            val action = listeFragmentDirections.actionListeFragmentToTarifFragment("menudengeldim",id = 0)
             Navigation.findNavController(this,R.id.fragmentContainerView).navigate(action)
-            // view içerisinde olduğumuzda 'finNavController(it)' demiştik ama şuan görünüm içinde değiliz.activity içerisindeyiz oyüzden activity'nin kendisini verebiliriz.
-            // activity'nin kendisini verince bir de parametre olarak viewID istedi.
-            // viewID olarak activity_main.xml içinde olan navHostFragment'ın ıd'sini vericez.
         }
-
-
         return super.onOptionsItemSelected(item)
-        //option menuden bir şey seçilirse ne yapayım buraya yazılır.
-
     }
-
-
-
-
-
 }
--- app/src/main/java/com/melikearikan/yemektarifleri_projesi/listeRecyclerAdapter.kt
+++ app/src/main/java/com/melikearikan/yemektarifleri_projesi/listeRecyclerAdapter.kt
@@ -11,36 +11,24 @@ import kotlinx.android.synthetic.main.recyclerrow.view.*
 
 class listeRecyclerAdapter(val yemekListe : ArrayList<String>,val IdList :ArrayList<Int>) : RecyclerView.Adapter<listeRecyclerAdapter.yemekHolder>() {
     class yemekHolder(itemView : View):RecyclerView.ViewHolder(itemView) {
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): yemekHolder {
        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.recyclerrow,parent,false) //parenta bağlıyorduk
+        val view = inflater.inflate(R.layout.recyclerrow,parent,false)
         return yemekHolder(view)
-        // recyclerrow xml'ini buraya bağladık.
-
     }
 
     override fun onBindViewHolder(holder: yemekHolder, position: Int) {
-        //recyclerrowlar içerisine ne koyucaz yazıyoruz
         holder.itemView.recycler_row_Text.text = yemekListe.get(position)
         holder.itemView.setOnClickListener {
-          // tarifFragmanına 2 tane bilgi yollicaz:
-          // 1)_ yeni yemek eklemek istediğimizde de, ya da recyclerView içerisinde olan yemekİsmini üzerine tıkladığımızda da yemekle alakalı detay vermek için yine tarifFragmenta gidicez.
-          // haliyle nereye tıklanarak tariffragment'a gidildi bilgisi göderilicek
-          // 2)_ eğer recyclerView içindeki yemek ismine takıldıysa tıklana yemeğe ait ıd bilgisi
-          // tarifFragment'a bilgi gödericeksek navigation_graphdan arguman vermen lazım.
-             var action = listeFragmentDirections.actionListeFragmentToTarifFragment("recyclerdangeldim",IdList.get(position))  // neryee basarak taifFragment'a geldik onu anlamak için tarifFragment'a giderken bilgi ve seçmek istenilen yemeğin bilgilerini almak için ıd gönderiyoruz
+             var action = listeFragmentDirections.actionListeFragmentToTarifFragment("recyclerdangeldim",IdList.get(position))
             Navigation.findNavController(it).navigate(action)
-            //tarifFragment içinde onviewVreated içinde nasıl oraya gittiğimiz bilgisini alıyorum.
         }
-
     }
 
     override fun getItemCount(): Int {
-        return yemekListe.size   // yemekListesi ya da IdList ferk etmez aynı sonucu veriir.
-        // recyclerrow kaç kere recyclerView içinde kullanılıcak ifade etttik
+        return yemekListe.size
 
     }
 }
